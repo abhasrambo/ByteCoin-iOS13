8ec4f77435280f8a17aec311c796f0e8f1362f74
Abhas Kumar <abhasroy@gmail.com>
2020-06-16 14:30:56 -0700

    Update CoinManager.swift

--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -2,8 +2,8 @@
 //  CoinManager.swift
 //  ByteCoin
 //
-//  Created by Angela Yu on 11/09/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Abhas Kumar on 19/05/2020.
+//  opyright © 2020 Abhas Kumar. All rights reserved.
 //
 
 import Foundation
